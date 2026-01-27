0e9e47cc704f049ce898727042341d5f5828ec9e
MaidaNadeem15 <maidanadeemf23@nutech.edu.pk>
2026-01-27 15:02:24 +0500

    Another update

--- fronted_ui/src/App.jsx
+++ fronted_ui/src/App.jsx
@@ -83,7 +83,7 @@ function App() {
 
       <div className="transcript">
         {text || <span className="placeholder">Your speech will appear here...</span>}
-        hii
+        hiiiiiii
       </div>
     </div>
   );
